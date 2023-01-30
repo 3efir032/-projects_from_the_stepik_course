946bee41a5e20be7d3d50741c4b82c6cdff6dbad
3efir032 <mitrofanov032@gmail.com>
2023-01-30 15:11:12 +0300

    pylint s_p_g.py

--- Secure_Password_Generator.py
+++ Secure_Password_Generator.py
@@ -1,5 +1,7 @@
-import random
-from string import *
+"""Программа для генерации паролей по выбранным параметрам пользователей."""
+
+from random import choice
+from string import digits, ascii_lowercase, ascii_uppercase, punctuation
 import math
 
 DIGITS = digits
@@ -10,15 +12,16 @@ AMBIGUOUS_CHARACTERS = 'il1Lo0O'
 
 
 def pre_setting_passwords():
+    """Функция для настройки паролей по выбранным параметрам пользователя."""
     password_setting_algorithm = []
     list_of_questions_for_setting_up_password = [
         'Количество паролей для генерации: ',
         'Длину одного пароля: ',
-        'Включать ли цифры 0123456789? (Добавьте +, если да и - если нет): ',
-        'Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? (Добавьте +, если да и - если нет): ',
-        'Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? (Добавьте +, если да и - если нет): ',
-        'Включать ли символы !#$%&*+-=?@^_? (Добавьте +, если да и - если нет): ',
-        'Исключать ли неоднозначные символы il1Lo0O? (Добавьте +, если да и - если нет): '
+        '\n(Добавьте +, если да и - если нет)\n\nВключать ли цифры 0123456789? ',
+        'Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? ',
+        'Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? ',
+        'Включать ли символы !#$%&*+-=?@^_? ',
+        'Исключать ли неоднозначные символы il1Lo0O? '
     ]
 
     count = 0
@@ -26,7 +29,7 @@ def pre_setting_passwords():
         input_text = input(list_of_questions_for_setting_up_password[count])
         if count <= 1 and not input_text.isdigit():
             print('Добавьте цифру от 1 до ...')
-        elif count >= 2 and (input_text != '+' and input_text != '-'):
+        elif count >= 2 and (input_text not in ('+', '-')):
             print('Добавьте + или -')
         else:
             password_setting_algorithm.append(input_text)
@@ -36,7 +39,8 @@ def pre_setting_passwords():
 
 
 def setting_the_number_of_passwords_and_character_selection():
-    pre_setting_password = pre_setting_passwords()  # [3, 14, '+', '+', '+', '+', '+']
+    """Функция генерации списка случайных символов по выбранным параметрам пользователя."""
+    pre_setting_password = pre_setting_passwords()
     list_variables = [DIGITS, UPPERCASE_LETTERS, LOWERCASE_LETTERS, PUNCTUATION, AMBIGUOUS_CHARACTERS]
     characters_for_the_password = ''
     for index, passwords in enumerate(pre_setting_password[2:-1]):
@@ -55,10 +59,12 @@ def setting_the_number_of_passwords_and_character_selection():
 
 
 def generation_passwords():
+    """Функция выбирает случайные символы из генерируемого списка случайных символов и
+    создает пароли из кол-ва указаной длины и кол-во паролей для генерации."""
     chars, quantity, length = setting_the_number_of_passwords_and_character_selection()
     passwords = []
     for i in range(quantity):
-        passwords.append([random.choice(chars) for _ in range(length)])
+        passwords.append([choice(chars) for _ in range(length)])
     return [''.join(passwords[i]) for i in range(quantity)]
 
 
